4a56e3bfea1ea15bb7d8ef2da13601e9601f546c
hrdk224 <hardikkille07@gmail.com>
2026-03-17 11:17:55 +0000

    add .forEach()

---
+++ Methods and Loops/03. The .forEach() Method/index.js
@@ -0,0 +1,26 @@
+const characters = [
+    {
+        title: 'Ninja',
+        emoji: '🥷',
+        powers: ['agility', 'stealth', 'aggression'],
+    },
+    {
+        title: 'Sorcerer',
+        emoji: '🧙',
+        powers: ['magic', 'invisibility', 'necromancy'],
+    },
+    {
+        title: 'Ogre',
+        emoji: '👹',
+        powers: ['power', 'stamina', 'shapeshifting'],
+    },
+    {
+        title: 'Unicorn',
+        emoji: '🦄',
+        powers: ['flight', 'power', 'purity'],
+    }
+]
+
+characters.forEach(function (character, index) {
+    console.log(index, character.title)
+})
