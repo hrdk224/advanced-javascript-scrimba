f131a28c8bfe424be9aaab27072a6c4ad3eed531
hrdk224 <hardikkille07@gmail.com>
2026-03-18 13:36:51 +0000

    add filter() method

---
+++ Methods and Loops/08. The .filter() Method/index.js
@@ -0,0 +1,18 @@
+const ages = [1, 5, 9, 23, 56, 10, 47, 10, 23, 18]
+
+const adults = ages.filter(function (age) {
+    return age >= 18
+})
+
+/*
+Challenge:
+1. Use the .filter method to create an array
+   called "children" which holds all of the ages
+   that are under 18.
+*/
+
+const children = ages.filter(function (age) {
+    return age < 18
+})
+
+console.log(adults)
