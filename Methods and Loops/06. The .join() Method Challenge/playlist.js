ec9212c21024ecff982626955dcac7e73844db34
hrdk224 <hardikkille07@gmail.com>
2026-03-18 11:16:41 +0000

    add .join() method

---
+++ Methods and Loops/06. The .join() Method Challenge/playlist.js
@@ -0,0 +1,25 @@
+export const playlistArr = [
+    {
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        albumArt: 'bohemian-rhapsody.png'
+    },
+
+    {
+        title: 'As It Was',
+        artist: 'Harry styles',
+        albumArt: 'as-it-was.png'
+    },
+
+    {
+        title: 'Stairway to Heaven',
+        artist: 'Led Zeppelin',
+        albumArt: 'stairway-to-heaven.png'
+    },
+
+    {
+        title: 'Therefore I Am',
+        artist: 'Billie Eilish',
+        albumArt: 'therefore-i-am.png'
+    }
+]
