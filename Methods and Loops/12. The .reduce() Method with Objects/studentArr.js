11adb99078fbff6eeb8319032eb5a7e36a55756f
hrdk224 <hardikkille07@gmail.com>
2026-03-24 11:15:35 +0000

    add .reduce() method with object

---
+++ Methods and Loops/12. The .reduce() Method with Objects/studentArr.js
@@ -0,0 +1,16 @@
+export const studentsArr = [
+    {
+        name: 'Mike',
+        grade: 75
+    }
+    ,
+    {
+        name: 'Emma',
+        grade: 75
+    }
+    ,
+    {
+        name: 'Seth',
+        grade: 75
+    }
+]
