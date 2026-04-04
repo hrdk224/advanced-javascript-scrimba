e787439a6325bb300cccac89c6394160186e0f59
hrdk224 <hardikkille07@gmail.com>
2026-04-04 06:17:21 +0000

    add regex flags

---
+++ Methods and Loops/15. Regex flags and constructor/index.js
@@ -0,0 +1,7 @@
+const text = "Please switch off the WIFI before you leave"
+const userInput = "WIFI"
+
+const regex = new RegExp(userInput, 'g')
+const doesMatch = regex.test(text) //boolean
+
+console.log(doesMatch)
